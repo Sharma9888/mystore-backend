eef281206f105757cefe84aad480383ef5fd9e21
Saksham <ssharma@innow8apps.com>
2022-05-03 15:01:15 +0530

    port changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use("/api/v1", router);
 
-const port = process.env.port;
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Server is successfully running on port:${port}.`);
